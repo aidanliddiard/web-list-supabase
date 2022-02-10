29730ee5ff79a62ea6674b7c3d2ad69e41a63226
aidanliddiard <aidanliddiard@gmail.com>
2022-02-09 16:41:21 -0800

    setting up supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://euyvcwbxbvsyohlvlxjk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1eXZjd2J4YnZzeW9obHZseGprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0NTMxNDEsImV4cCI6MTk2MDAyOTE0MX0.2SIvdAPNpzyD2t_5zjQNhqc-WzDTJc5iAdX7Bws1Tc0';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
