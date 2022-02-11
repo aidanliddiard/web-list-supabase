79584973d7639ffcc0311d5693ef92efe8dda6f9
aidanliddiard <aidanliddiard@gmail.com>
2022-02-11 12:46:32 -0800

    adding css

--- test/utils.test.js
+++ test/utils.test.js
@@ -29,7 +29,6 @@ test('time to test renderCountries function', (expect) => {
 test('time to test renderCountryDetails function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    //const expected = '<div class="countries"><img src="/assets/icons8-australia-96.png"><h3>Australia Facts</h3><ul><li>Population: 25.69 million</li><li>Capital: Canberra</li><li>Fun Fact: If you were to visit one new beach in Australia every day, it would take more than 27 years to see them all.</li></ul> <iframe src="https://www.google.com/maps/embed?pb=!4v1644602448305!6m8!1m7!1sTNE1NPAnEUNYhGk9kFWyJw!2m2!1d-23.30339107314864!2d151.9149548463482!3f287.37!4f-18.959999999999994!5f0.7820865974627469" </iframe></div>';
     const expected = '<div class="countries"><img src="/assets/icons8-australia-96.png"><h3>Australia Facts</h3><ul><li>Population: 25.69 million</li><li>Capital: Canberra</li><li>Fun Fact: If you were to visit one new beach in Australia every day, it would take more than 27 years to see them all.</li></ul><iframe src="https://www.google.com/maps/embed?pb=!4v1644602448305!6m8!1m7!1sTNE1NPAnEUNYhGk9kFWyJw!2m2!1d-23.30339107314864!2d151.9149548463482!3f287.37!4f-18.959999999999994!5f0.7820865974627469"></iframe></div>';
     
     //Act 
