d30d6ff8ff3e298a6d8f71fdca8fb026a9b6966e
aidanliddiard <aidanliddiard@gmail.com>
2022-02-10 16:00:04 -0800

    tests passing

--- fetch-utils.js
+++ fetch-utils.js
@@ -7,3 +7,7 @@ function checkError({ data, error }) {
     return error ? console.error(error) : data;
 }
 
+export async function getCountries() {
+    const resp = await client.from('')
+}
+
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,29 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderCountryDetails } from '../render-utils.js';
+import { renderCountries, renderCountryDetails } from '../render-utils.js';
 
 const test = QUnit.test;
+
+test('time to test renderCountries function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = '<div class="country"><img src="/assets/icons8-australia-96.png"><h3>Australia Facts</h3></div>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderCountries ({
+        name: 'Australia',
+        img: '/assets/icons8-australia-96.png',
+        population: '25.69 million',
+        capital: 'Canberra',
+        fact: 'If you were to visit one new beach in Australia every day, it would take more than 27 years to see them all.',
+    });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
 
 test('time to test renderCountryDetails function', (expect) => {
     //Arrange
@@ -12,9 +32,17 @@ test('time to test renderCountryDetails function', (expect) => {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderCountryDetails ();
+    const actual = renderCountryDetails ({
+        name: 'Australia',
+        img: '/assets/icons8-australia-96.png',
+        population: '25.69 million',
+        capital: 'Canberra',
+        fact: 'If you were to visit one new beach in Australia every day, it would take more than 27 years to see them all.',
+    });
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
+
+
